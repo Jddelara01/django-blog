b499c6ddd44a44217f8e0c7fde1090ebb84f4f9d
Jddelara01 <jddelara01@gmail.com>
2023-03-08 09:12:44 +0000

    Add custom models for DB

--- codestar/settings.py
+++ codestar/settings.py
@@ -132,10 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'  # noqa
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
